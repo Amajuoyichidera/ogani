379762593c6d8a06869ec9abf4046d8407495f5e
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2024-06-11 22:30:14 +0100

    first version

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import Logo from '../assets/logo.png'
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+  return (
+    <nav className="navbar">
+            {/* <div className="navbar-logo">Logo</div> */}
+            <img src={Logo} />
+            <div className={`navbar-menu ${isOpen ? 'open' : ''}`}>
+                <a href="#home" className="navbar-item">Home</a>
+                <a href="#about" className="navbar-item">About</a>
+                <a href="#services" className="navbar-item">Services</a>
+                <a href="#contact" className="navbar-item">Contact</a>
+            </div>
+            <div className="navbar-toggle" onClick={toggleMenu}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+        </nav>
+  )
+}
+
+export default Navbar
---
+++ src/components/vege/Vege.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import { FaAngleUp, FaAngleDown } from "react-icons/fa6";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import Banner from '../../assets/banner.jpg';
+import styles from './vege.module.css';
+
+
+const Vege = () => {
+    const [showDepart, setShowDepart] = useState(true);
+    const department = ['Fresh Meat',
+        'Vegetables',
+        'Fruit & Nut Gifts',
+        'Fresh Berries',
+        'Ocean Foods',
+        'Butter & Eggs',
+        'Fastfood',
+        'Fresh Onion',
+        'Papayaya & Crisps',
+        'Oatmeal',
+        'Fresh Bananas']
+  return (
+    <div className={styles.vege}>
+        <div>
+            <section onClick={() => setShowDepart(!showDepart)} className={styles.allDepart}>
+                <h4>All Departments</h4>
+                <section>{showDepart ? <FaAngleDown /> : <FaAngleUp />}</section>
+            </section>
+            {showDepart && <section className={styles.depart}>
+                {department.map((item) => <h5>{item}</h5>)}
+            </section>}
+        </div>
+
+
+        <div>
+            <div className={styles.allCat}>
+            <section className={styles.search}>
+            <section className={styles.sec1}>
+                <section className={styles.down}>
+                    <h4>All Categories</h4>
+                    <FaAngleDown />
+                </section>
+                <div className={styles.line}></div>
+                <input placeholder='What do you need?' />
+            </section>
+            <button>SEARCH</button>
+            </section>
+
+            <section className={styles.number}>
+             <section className={styles.tele}><BsFillTelephoneFill /></section>
+             <section>
+                <h3>+65 11.188.888</h3>
+                <p>support 24/7 time</p>
+             </section>
+            </section>
+            </div>
+
+            <div className={styles.imgCon}>
+                <section className={styles.overText}>
+                    <h5>FRUIT FRESH</h5>
+                    <h1>Vegetable <br />
+                    100% Organic</h1>
+                    <p>Free Pickup and Delivery Available</p>
+                    <button>Shop Now</button>
+                </section>
+                    <img className={styles.background} src={Banner} />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Vege
